8e81d34b8171d210dd1ec275618d330c7544b43c
Alejandro Villar <avillar@ticnor.es>
2022-11-29 12:58:11 -0500

    WIP Backend working

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,24 +1,91 @@
-from distutils.command.upload import upload
-
-from fastapi import FastAPI, Body, HTTPException
-from jsonschema.exceptions import ValidationError as JsonValidationError
+import json
+import logging
+import os
+import re
+import requests
+from pathlib import Path
+from fastapi import FastAPI, HTTPException, File, Form, Response
+from fastapi.middleware.cors import CORSMiddleware
 from ogc.na import ingest_json
+
+logging.config.fileConfig(Path(__file__).parent / 'logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger('ogc_playground')
+
+CORS_ALLOW_ORIGINS = os.environ.get('CORS_ALLOW_ORIGINS', '*').split(',')
+
+rfa = os.environ.get('REMOTE_FETCH_ALLOWED', '').strip()
+if rfa:
+    REMOTE_FETCH_ALLOWED = re.compile(rfa)
+    logger.info('Remote fetch allowed for "%s"', rfa)
+else:
+    REMOTE_FETCH_ALLOWED = None
+    logger.info('Remote fetch disabled')
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+logger.info('Allowed CORS origins: %s', CORS_ALLOW_ORIGINS)
+
+
+async def _remote_fetch(url: str) -> bytes | bool:
+    if not REMOTE_FETCH_ALLOWED or not REMOTE_FETCH_ALLOWED.fullmatch(url):
+        logger.warning('Remote fetch not allowed for %s', url)
+        return False
+    logger.info('Remote fetching %s', url)
+    r = requests.get(url)
+    r.raise_for_status()
+    return r.content
+
 
 @app.post("/json-uplift")
-async def json_uplift(context: str | dict = Body(), jsondoc: dict = Body(), output: str | None = None,
-               base: str | None = None):
+async def json_uplift(context: bytes = File(''),
+                      contexturl: str | None = Form(None),
+                      jsondoc: bytes = File('', alias='json'),
+                      jsonurl: str | None = Form(None),
+                      output: str | None = Form(None),
+                      base: str | None = Form(None)):
     try:
+        if isinstance(context, bytes):
+            context = context.decode('utf-8')
+        if not context and contexturl:
+            context = await _remote_fetch(contexturl)
         context = ingest_json.validate_context(context=context)
+
+        if not jsondoc:
+            if jsonurl:
+                jsondoc = await _remote_fetch(jsonurl)
+            else:
+                raise HTTPException(
+                    status_code=422,
+                    detail={
+                        "type": "UnprocessableEntity",
+                        "msg": "No JSON source specified",
+                    }
+                )
+        jsondoc = json.loads(jsondoc)
         g, expanded, uplifted = ingest_json.generate_graph(jsondoc, context, base)
         if output == 'ttl':
-            return g.serialize(format='ttl')
+            return Response(g.serialize(format='ttl'), media_type="text/turtle")
         elif output == 'expanded':
             return expanded
         else:
             return uplifted
+    except HTTPException:
+        raise
+    except json.JSONDecodeError:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "type": "JSONDecodeError",
+                "msg": "Invalid JSON input",
+            }
+        )
     except ingest_json.ValidationError as e:
         raise HTTPException(
             status_code=400,
