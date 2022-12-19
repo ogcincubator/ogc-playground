c8dcd93c351427c0292aaa713ba135979b7eb7ab
Alejandro Villar <avillar@ticnor.es>
2022-12-19 14:18:58 +0100

    Edit frontend base from Docker entrypoing

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -7,6 +7,9 @@ printenv | grep ^VUE_APP_ | while read APP_ENV_VAR; do
   sed -r "s@window.ogcPlayground.${VAR_NAME}.*@window.ogcPlayground.${VAR_NAME} = \"${VAR_VALUE}\";@" -i /app/env-config.js
 done
 
+sed -r 's@<base[^>]+>@@' -i /app/index.html
+sed -r "s@</head>@<base href="${VUE_APP_SERVE_PATH}"></head>@" -i /app/index.html
+
 envsubst '${VUE_APP_SERVE_PATH}' < /etc/nginx/nginx.conf.tpl > /etc/nginx/nginx.conf
 rm -rf /usr/share/nginx/html/*
 mkdir -p /usr/share/nginx/html${VUE_APP_SERVE_PATH}
