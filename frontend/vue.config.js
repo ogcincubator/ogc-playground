8d33e9ff7e4cdcd1ba9a07edf723d224dcdaf34e
Alejandro Villar <avillar@ogc.org>
2023-05-25 12:52:53 +0200

    Set HTML title

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,32 +1,41 @@
 const {defineConfig} = require('@vue/cli-service');
 const CopyPlugin = require("copy-webpack-plugin");
 module.exports = defineConfig({
-    publicPath: 'auto',
-    transpileDependencies: true,
+  publicPath: 'auto',
+  transpileDependencies: true,
 
-    pluginOptions: {
-        vuetify: {
-            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-        }
-    },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'OGC Playground';
+        return args;
+      });
+  },
 
-    configureWebpack: {
-        plugins: [
-            new CopyPlugin({
-                patterns: [{
-                    from: "src/env-config.js",
-                    to: "env-config.js",
-                    transform(content) {
-                        const envConf = Object.entries(process.env)
-                            .filter(([key]) => key.startsWith('VUE_APP_'))
-                            .reduce(
-                                (acc, cur) =>
-                                    `${acc}\n${cur[0].replace(/^VUE_APP_/, 'window.ogcPlayground.')}=${JSON.stringify(cur[1])};`, '');
-                        return content.toString().replace('//OGC_PLAYGROUND_CONFIG', envConf);
-                    },
-                    info: { minimized: true },
-                }],
-            }),
-        ],
-    },
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    }
+  },
+
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin({
+        patterns: [{
+          from: "src/env-config.js",
+          to: "env-config.js",
+          transform(content) {
+            const envConf = Object.entries(process.env)
+              .filter(([key]) => key.startsWith('VUE_APP_'))
+              .reduce(
+                (acc, cur) =>
+                  `${acc}\n${cur[0].replace(/^VUE_APP_/, 'window.ogcPlayground.')}=${JSON.stringify(cur[1])};`, '');
+            return content.toString().replace('//OGC_PLAYGROUND_CONFIG', envConf);
+          },
+          info: {minimized: true},
+        }],
+      }),
+    ],
+  },
 })
